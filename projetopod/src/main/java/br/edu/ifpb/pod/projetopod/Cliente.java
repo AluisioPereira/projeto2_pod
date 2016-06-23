d826cd66beb064f5c3f8f4ecd3601d8a01034823
Wellington <Wellington@joe_pc>
2016-06-23 17:30:23 -0300

    Tudo ok

--- projetopod/src/main/java/br/edu/ifpb/pod/projetopod/Cliente.java
+++ projetopod/src/main/java/br/edu/ifpb/pod/projetopod/Cliente.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.pod.projetoconexao;
+package br.edu.ifpb.pod.projetopod;
 
 import java.io.IOException;
 import java.io.InputStream;
---
+++ projetopod/src/main/java/br/edu/ifpb/pod/projetopod/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.pod.projetopod;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author laerton
+ */
+public class Main {
+    private static Regras regras;
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        /*Servidor server = new Servidor(1099);
+        Person pessoa = server.getPerson();
+        
+        */
+        Servidor server = new Servidor(1099);
+         //processa a mensagem e registra os dados no banco
+        server.processaMensagem();
+        try {
+            Cliente client = new Cliente("localhost", 1097);
+            //Verifica se a mensagem tem dados para encaminhar para node 1 e encaminha
+            server.encaminhaMensagem(client);
+        } catch (Exception e) 
+        {
+            System.err.println(e.getMessage()); 
+        }
+        
+    }
+    
+}
