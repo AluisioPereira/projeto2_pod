5850034849d9355399c5613eff171079f902fd13
Wellington <Wellington@joe_pc>
2016-06-30 19:56:27 -0300

    Criand o node2

---
+++ POD1/node2/src/main/java/br/edu/ifpb/pod/node2/Main.java
@@ -0,0 +1,25 @@
+package br.edu.ifpb.pod.node2;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Document Main
+ * @Date  30/06/2016 @Time 19:52:54
+ * @author Wellington Lins Claudino Duarte   
+ * @mail wellingtonlins2013@gmail.com
+ */ 
+public class Main {
+   
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        /*
+        //Enviando dados de persoa 
+        Person pessoa = new Person(1, "Joe", "35222786");
+        Cliente node1 = new Cliente("10.3.188.193", 1099);
+        node1.enviaMensagem(pessoa.toString());
+        //Cliente node3 = new Cliente("x.x.x.x", 1099);
+*/
+    }
+    
+}
