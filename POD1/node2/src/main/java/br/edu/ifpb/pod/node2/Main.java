cd120b361b010af3f0860c53c4b0af8bc1188558
Wellington <Wellington@joe_pc>
2016-06-30 21:30:56 -0300

    Mandando mensagens !!! Topico 2  e  Topico 3

--- POD1/node2/src/main/java/br/edu/ifpb/pod/node2/Main.java
+++ POD1/node2/src/main/java/br/edu/ifpb/pod/node2/Main.java
@@ -5,21 +5,27 @@ import java.sql.SQLException;
 
 /**
  * Document Main
- * @Date  30/06/2016 @Time 19:52:54
- * @author Wellington Lins Claudino Duarte   
+ *
+ * @Date 30/06/2016 @Time 19:52:54
+ * @author Wellington Lins Claudino Duarte
  * @mail wellingtonlins2013@gmail.com
- */ 
+ */
 public class Main {
-   
-    
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-        /*
-        //Enviando dados de persoa 
-        Person pessoa = new Person(1, "Joe", "35222786");
-        Cliente node1 = new Cliente("10.3.188.193", 1099);
-        node1.enviaMensagem(pessoa.toString());
-        //Cliente node3 = new Cliente("x.x.x.x", 1099);
-*/
+        Cliente cliente = new Cliente("127.0.0.1", 12345);
+        Mensagem mensagem = new Mensagem();
+        mensagem.setRemetente("Node2");
+        mensagem.setTopico("Topico 2");
+        mensagem.setTexto("Qual o significado da existencia..");
+        cliente.enviaMensagem(mensagem);
+        
+        Mensagem mensagem2 = new Mensagem();
+        mensagem2.setRemetente("Node2");
+        mensagem2.setTopico("Topico 3");
+        mensagem2.setTexto("Quem nasceu primeiro a ave ou o lagarto?");
+        cliente.enviaMensagem(mensagem);
+
     }
-    
+
 }
